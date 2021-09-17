c939eb6cc7d9f1cd615aa1de6b03efc6ceacb1f1
Jonathan Bayer <linuxgurugamer@gmail.com>
2021-09-17 13:28:19 -0400

    Changed name of DLL for CKAN compatibility

--- ProbeControlRoom/AssemblyVersion.cs
+++ ProbeControlRoom/AssemblyVersion.cs
@@ -5,5 +5,5 @@
   
  using System.Reflection;
 
- [assembly: AssemblyVersion("1.2.3.1")]
- [assembly: AssemblyFileVersion("1.2.3.1")]
+ [assembly: AssemblyVersion("1.2.4.1")]
+ [assembly: AssemblyFileVersion("1.2.4.1")]
